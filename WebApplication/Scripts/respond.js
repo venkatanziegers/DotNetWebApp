e3f3d508137a752e00d2a978798a877676f52fba
venkatanziegers <venkatanziegers@gmail.com>
2021-10-28 19:23:54 +0530

    Update respond.js

--- WebApplication/Scripts/respond.js
+++ WebApplication/Scripts/respond.js
@@ -10,7 +10,7 @@
  * Microsoft neither took the code under GPL nor distributes it thereunder but
  * under the terms set out in this paragraph. All notices and licenses
  * below are for informational purposes only.
- * test 1
+ * test 123
  * NUGET: END LICENSE TEXT */
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 /*! NOTE: If you're already including a window.matchMedia polyfill via Modernizr or otherwise, you don't need this part */
