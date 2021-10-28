f93599eb69347a0ed4360c27e6740d0d7c8c9472
venkatanziegers <venkatanziegers@gmail.com>
2021-10-28 23:45:05 +0530

    Update jquery.validate-vsdoc.js

--- WebApplication/Scripts/jquery.validate-vsdoc.js
+++ WebApplication/Scripts/jquery.validate-vsdoc.js
@@ -31,7 +31,7 @@
 *
 * jQuery Validation Plugin - v1.11.1 - 2/4/2013
 * https://github.com/jzaefferer/jquery-validation
-* Copyright (c) 2013 Jörn Zaefferer; Licensed MIT
+* Copyright (c) 2013 Jörn Zaefferer; Licensed MIT DDD
 *
 */
 
