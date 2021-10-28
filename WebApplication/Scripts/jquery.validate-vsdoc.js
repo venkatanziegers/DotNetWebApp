b574eb1368c7aaeb60b90930806af85e363d5fe7
venkatanziegers <venkatanziegers@gmail.com>
2021-10-28 23:39:42 +0530

    Update jquery.validate-vsdoc.js

--- WebApplication/Scripts/jquery.validate-vsdoc.js
+++ WebApplication/Scripts/jquery.validate-vsdoc.js
@@ -6,7 +6,7 @@
  * terms; or (ii) using the files as included with a Microsoft product subject
  * to that product's license terms. Microsoft reserves all other rights to the
  * files not expressly granted by Microsoft, whether by implication, estoppel
- * or otherwise. Insofar as a script file is dual licensed under GPL,
+ * or otherwise. Insofar as a script file is dual licensed under GPL, SSSSSSSS
  * Microsoft neither took the code under GPL nor distributes it thereunder but
  * under the terms set out in this paragraph. All notices and licenses
  * below are for informational purposes only.
