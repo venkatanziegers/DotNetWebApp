379b23428511b6f0c8df7a031d4cd63cbde4e62c
venkatanziegers <venkatanziegers@gmail.com>
2021-10-28 23:42:13 +0530

    Update jquery.validate-vsdoc.js

--- WebApplication/Scripts/jquery.validate-vsdoc.js
+++ WebApplication/Scripts/jquery.validate-vsdoc.js
@@ -11,7 +11,7 @@
  * under the terms set out in this paragraph. All notices and licenses
  * below are for informational purposes only.
  *
- * NUGET: END LICENSE TEXT */
+ * NUGET: END LICENSE TEXT */XXXXXXXXX
 /*
 * This file has been commented to support Visual Studio Intellisense.
 * You should not use this file at runtime inside the browser--it is only
